86ca7123d60a3cdf1fa18a6650bc2708a47adef8
naker90 <AntonioJesusSG96@gmail.com>
2017-04-18 18:19:51 +0100

    Añadido transiciones a la pagina de about

--- src/components/about/aboutPage.js
+++ src/components/about/aboutPage.js
@@ -3,6 +3,22 @@
 var React = require("react");
 
 var About = React.createClass({
+
+    statics: {
+        willTransitionTo: function(transition, params, query, callback) {
+            if(!confirm("Estas seguro que quieres ir a esta pagina tan aburrida?")){
+                transition();
+            }else{
+                callback();
+            }
+        },
+        willTransitionFrom: function(transition, component) {
+            if(!confirm("Estas seguro que quieres salir de una pagina tan emocionante?")){
+                transition();
+            }
+        }
+    },
+
    render: function () {
        return (
          <div>
