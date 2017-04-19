2cf687c97ab21693b16efe75d0fc181f0139ace4
naker90 <AntonioJesusSG96@gmail.com>
2017-04-19 19:05:14 +0100

    Controlled components

--- src/components/authors/authorForm.js
+++ src/components/authors/authorForm.js
@@ -8,11 +8,13 @@ var AuthorForm = React.createClass({
            <div>
                <label>Nombre</label>
                <input type="text" name="firstname" className="form-control"
-                      placeholder="Nombre del autor"/>
+                      placeholder="Nombre del autor" value={this.props.author.firstName}
+                      onChange={this.props.onChange}/>
                <br/>
                <label>Apellidos</label>
                <input type="text" name="lastname" className="form-control"
-                      placeholder="Apellidos del autor"/>
+                      placeholder="Apellidos del autor" value={this.props.author.lastName}
+                      onChange={this.props.onChange}/>
                <br/>
                <input type="submit" className="btn btn-info" value="Guardar"/>
            </div>
--- src/components/authors/manageAuthorPage.js
+++ src/components/authors/manageAuthorPage.js
@@ -4,11 +4,27 @@ var React = require('react');
 var AuthorForm = require('./authorForm');
 
 var ManageAuthorPage = React.createClass({
+    getInitialState: function() {
+        return{
+            author: { id: "", firstName: "", lastName: "" }
+        };
+    },
+
+   setAuthorState: function (event) {
+       var field = event.target.name;
+       var value = event.target.value;
+       this.state.author[field] = value;
+       return this.state({author: this.state.author});
+   },
+
    render: function () {
         return (
             <div>
                 <h2>Gestor de autores</h2>
-                <AuthorForm/>
+                <AuthorForm
+                    author = {this.state.author}
+                    onChange={this.setAuthorState}
+                />
             </div>
         );
    }
